4f4b1361defb10b337a05637d465abbd522c5972
Parthiban <136749124+Parthideveloper@users.noreply.github.com>
2024-10-01 21:00:38 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function imgslider(anything){
+        const pepsi =  document.querySelector('.pepsi').src = anything;
+
+}
+
+function changeBgColor(color){
+          const sec = document.querySelector('.sec')
+          sec.style.background = color;
+
+
+}
